0292151076caafceb07dc4fe06f9d432280dca3b
HwangSeo <sadclara0830@naver.com>
2024-10-31 00:15:23 +0900

    회의 일정 화면 ui, 팝업 dialog, 리사이클러뷰 구현

--- app/src/main/java/com/example/kauplus/MainActivity.kt
+++ app/src/main/java/com/example/kauplus/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.kauplus
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kauplus.databinding.ActivityMainBinding
+import com.example.kauplus.ui.meeting.ScheduleFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding=ActivityMainBinding.inflate(layoutInflater)
+
+        binding.btn.setOnClickListener {
+            supportFragmentManager.beginTransaction().run{
+                replace(binding.fram.id,ScheduleFragment())
+                commit()
+            }
+        }
+
         setContentView(binding.root)
+
     }
 }
--- app/src/main/java/com/example/kauplus/ui/meeting/ScheduleFragment.kt
+++ app/src/main/java/com/example/kauplus/ui/meeting/ScheduleFragment.kt
@@ -1,13 +1,19 @@
 package com.example.kauplus.ui.meeting
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kauplus.R
 import com.example.kauplus.databinding.FragmentScheduleBinding
 
 class ScheduleFragment : Fragment() {
+
+    private var itemList: ArrayList<Meeting> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,9 +24,38 @@ class ScheduleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding=FragmentScheduleBinding.inflate(inflater,container,false)
-        return binding.root
 
         //대부분의 작업 수행
+        itemList= arrayListOf(
+            Meeting("프로젝트 X 8주차 회의","14:00~15:00","C1 스터디룸"),
+            Meeting("산학 2주차 회의","13:00~14:00","전자관 413호")
+        )
+
+        val scheduleRVAdapter = ScheduleRVAdapter(itemList)
+        val recyclerView = binding.rvMeeting
+        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+        recyclerView.adapter = scheduleRVAdapter
+
+        scheduleRVAdapter.setMyItemClickListener(object :ScheduleRVAdapter.MyItemClickListener {
+
+            override fun onItemClick(position: Int) {
+                val bottomSheet = MeetingDetailBottomSheet()
+                bottomSheet.show(parentFragmentManager, MeetingDetailBottomSheet().tag)
+            }
+
+        })
+        binding.textReservedMeeting.setOnClickListener {
+            binding.textReservedMeeting.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            binding.textClosedMeeting.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray))
+            binding.btnWriteMeeting.visibility=View.VISIBLE
+        }
+        binding.textClosedMeeting.setOnClickListener {
+            binding.textReservedMeeting.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray))
+            binding.textClosedMeeting.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            binding.btnWriteMeeting.visibility=View.GONE
+        }
+        return binding.root
+
     }
 
 }
