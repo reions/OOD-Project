08a507c8774abbe944e8b8e721394e8dfe300487
HwangSeo <sadclara0830@naver.com>
2024-11-23 01:09:04 +0900

    종료되면 팝업 사라짐 구현, ui 수정

--- app/src/main/java/com/example/kauplus/ui/meeting/MeetingDetailBottomSheet.kt
+++ app/src/main/java/com/example/kauplus/ui/meeting/MeetingDetailBottomSheet.kt
@@ -88,6 +88,7 @@ class MeetingDetailBottomSheet : BottomSheetDialogFragment() {
                 }
                 binding.btnClose.setOnClickListener {
                     viewModel.closeMeeting(meetingId)
+                    dismiss()
                 }
             }
         }
