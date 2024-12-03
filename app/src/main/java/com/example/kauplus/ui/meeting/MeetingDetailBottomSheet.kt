c3e6a8e7000cf66d0592a24cdb27dd5a630a46e0
HwangSeo <sadclara0830@naver.com>
2024-12-03 14:54:16 +0900

    바텀시트 마크 추가

--- app/src/main/java/com/example/kauplus/ui/meeting/MeetingDetailBottomSheet.kt
+++ app/src/main/java/com/example/kauplus/ui/meeting/MeetingDetailBottomSheet.kt
@@ -39,8 +39,8 @@ class MeetingDetailBottomSheet : BottomSheetDialogFragment() {
                 val toDo=it.toDo
 
                 binding.title.text = it.title
-                binding.time.text = it.time
-                binding.place.text = it.place
+                binding.time.text = "\u2022 ${it.time}"
+                binding.place.text = "\u2022 ${it.place}"
 
                 Log.d("이미지", it.img1.toString())
                 Log.d("이미지", it.img2.toString())
